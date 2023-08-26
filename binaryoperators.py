a829574e55dbb65a142fe2483326a97ee575c2aa
Shrey008 <115262845+Shrey008@users.noreply.github.com>
2023-08-27 02:38:32 +0530

    Add files via upload

---
+++ binaryoperators.py
@@ -0,0 +1,46 @@
+q = 5
+n =3
+a = q + n
+print(a)
+
+b = q - n
+c = q*n
+print(b)
+print(c)
+
+# % it prints the remainder
+
+module = q % n
+print(module)
+
+#if we print a small number to a big number it will print the divisor 
+# which is 3 it wont print the remainder
+modulo = n % q
+print(modulo)
+
+
+#if we perform division operation answer will be in float always unless // is used
+division = q / n
+print(division)
+
+#implicit conversion
+
+divide = 5 /5  
+print(divide)
+
+#for converting it into float // is used
+
+#floor division
+modulo = n % q
+print(modulo)
+
+# here // in division with float produces the output as float but also as floor value 
+m = 5.5 // 3
+print(m)
+
+
+# it print the power of 1st value and which the second value it his power (or in exponential form)
+power = q**n
+print(power)
+
+print(2-3 + 2*12)
---
+++ datatype.py
@@ -0,0 +1,43 @@
+n = 12
+print(n)
+print(type(n))
+
+n = 12.2
+print(n)
+print(type(n))
+
+n = 1 + 3j
+print(n)
+print(type(n))
+
+
+#booleans - true or false
+
+isValid = True
+print(isValid)
+print(type(isValid))
+
+name="shrey"
+print(name)
+print(type(name))
+
+#none
+n="hello"
+n = None
+print(type(n))
+
+
+# integer and integer = integer
+print(1 + 2)
+
+# integer and float = float
+
+print(1+2.0)
+
+# integer float and complex will give output =  complex
+print(3+2.0+4j)
+
+
+
+
+
